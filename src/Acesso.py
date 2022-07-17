b420ffd578cc04e2dd8aa2befcd7dad92cae2e1a
Washington Bispo <juniorbispo515@gmail.com>
2022-07-16 22:01:19 -0300

    Adicionando rótulos de testes
    Co-authored-by: Emily <diassousa@yahoo.com.br>
    Co-authored-by: eddie-gomes <edvan.bgjunior@gmail.com>
    Co-authored-by: ThiagoGMF <thiago.guilherme20@hotmail.com>
    Co-authored-by: philipeserafim <philipeserafim@gmail.com>

--- src/Acesso.py
+++ src/Acesso.py
@@ -1,5 +1,14 @@
+from src.Exceptions import DescricaoEmBrancoException
+
+
 class Acesso():
     def __init__(self, placa, horaEntrada, horaSaida):
-        self.placa = placa 
+        if(placa == ""):
+            raise DescricaoEmBrancoException
+        if(horaEntrada == ""):
+            raise DescricaoEmBrancoException
+        if(horaSaida == ""):
+            raise DescricaoEmBrancoException    
+        self.placa = placa
         self.horaEntrada = horaEntrada 
         self.horaSaida = horaSaida
--- tests/estacionamento_test.py
+++ tests/estacionamento_test.py
@@ -12,6 +12,7 @@ estacionamento = [
                    valorEvento=40, horarios=(6, 22, 20, 8, 20, 8), capacidade=600, retorno=0.7),
 ]
 
+@pytest.mark.funcional
 def testaCadastroUmVeiculo():
     estacionamento[0].AddAcesso('HI139', '08:30', '08:56')
     estacionamento[0].AddAcesso('G49NG', 'Mensalista', 'Mensalista')
@@ -21,23 +22,28 @@ def testaCadastroUmVeiculo():
 
     assert (estacionamento[0].getAcessos()) != 0
 
+@pytest.mark.funcional
 @pytest.mark.parametrize('expected, values',[('Noturna',['RM3A9', '20:00', '07:00']),('Mensalista',['G49NG', 'Mensalista', 'Mensalista']),('0:26',['HI139', '08:30', '08:56']),
 ('Evento',['AM31J', 'Evento', 'Evento'])])
 def testaTipoAcessoPorPlaca1(expected, values):
     estacionamento[0].AddAcesso(values[0], values[1], values[2])
     assert estacionamento[0].FindTipoAcesso(values[0]) == expected
 
+@pytest.mark.funcional
 @pytest.mark.parametrize('expected, values',[(50, ['AM31J', 'Evento', 'Evento']),(54,['RM3A9', '20:00', '07:00']),
 (600,['G49NG', 'Mensalista', 'Mensalista']),(60,['HI139', '08:30', '08:56'])])
 def testaValorAcesso(expected, values):
     estacionamento[0].AddAcesso(values[0], values[1], values[2])
     assert estacionamento[0].GetValorAcesso(values[0]) == expected
 
-
+@pytest.mark.funcional
 @pytest.mark.parametrize('expected, values', [(25,['AM31J','Evento', 'Evento']),(27,['RM3A9', '20:00', '07:00']),(300,['G49NG', 'Mensalista', 'Mensalista']),
 (30,['HI139', '08:30', '08:56']),(45,['AM321', '8:00','8:45'])])
 def testaValorContratante(expected, values):
     estacionamento[0].AddAcesso(values[0], values[1], values[2])
     assert estacionamento[0].GetValorContratante(values[0]) == expected
+
+
+
 
 
