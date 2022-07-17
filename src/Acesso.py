6463855bca6441a73be63a60ad8e6dc0cbb9ce98
Edvan Gomes <edvan.bgjunior@gmail.com>
2022-07-16 23:12:37 -0300

    Adicionando testes de exceção de valores inválidos
    Co-authored-by: Emily <diassousa@yahoo.com.br>
    Co-authored-by: WashingtonBispo <juniorbispo515@gmail.com>
    Co-authored-by: ThiagoGMF <thiago.guilherme20@hotmail.com>
    Co-authored-by: philipeserafim <philipeserafim@gmail.com>

--- src/Acesso.py
+++ src/Acesso.py
@@ -1,14 +1,37 @@
-from src.Exceptions import DescricaoEmBrancoException
-
+from src.Exceptions import DescricaoEmBrancoException, ValorAcessoInvalidoException
+import datetime
+import re
 
 class Acesso():
+
+    def is_time_valid(self, horario):
+
+        if not horario[0].isnumeric():
+            return True
+
+        hora = int(horario.split(":")[0])
+        minuto = int(horario.split(":")[1])
+
+        return hora >= 0 and hora <= 23 and minuto >= 0 and minuto <= 59
+
     def __init__(self, placa, horaEntrada, horaSaida):
         if(placa == ""):
-            raise DescricaoEmBrancoException
+            raise DescricaoEmBrancoException('placa')
         if(horaEntrada == ""):
-            raise DescricaoEmBrancoException
+            raise DescricaoEmBrancoException('horaEntrada')
         if(horaSaida == ""):
-            raise DescricaoEmBrancoException    
+            raise DescricaoEmBrancoException('horaSaida')
+
+        if(re.match("((0-9)(a-z)(A-Z)){5}",placa)):
+            raise ValorAcessoInvalidoException('Placa inválida!')
+        
+        if not self.is_time_valid(horaEntrada):
+            raise ValorAcessoInvalidoException('O valor de horaEntrada não é um valor válido')
+            
+        if not self.is_time_valid(horaSaida):
+            raise ValorAcessoInvalidoException('O valor de horaSaida não é um valor válido')
+        
         self.placa = placa
         self.horaEntrada = horaEntrada 
         self.horaSaida = horaSaida
+
--- tests/exception_test.py
+++ tests/exception_test.py
@@ -1,15 +1,33 @@
 from src.Acesso import Acesso
 from src.Exceptions import DescricaoEmBrancoException
+from src.Exceptions import ValorAcessoInvalidoException
 import pytest
 
+@pytest.mark.excecao
 def testePlacaBranco():
     with pytest.raises(DescricaoEmBrancoException):
         Acesso("","19:30",'20:30')
 
+@pytest.mark.excecao
 def testeEntradaBranco():
     with pytest.raises(DescricaoEmBrancoException):
         Acesso("AM319","",'20:34')
 
+@pytest.mark.excecao
 def testeSaidaBranco():
     with pytest.raises(DescricaoEmBrancoException):
         Acesso("AM122","20:34",'')
+
+@pytest.mark.excecao
+def testeValoresInvalidos():
+    with pytest.raises(ValorAcessoInvalidoException):
+        Acesso("AM12","25:00",'20:34')
+
+@pytest.mark.excecao
+def testeValoresInvalidos():
+    with pytest.raises(ValorAcessoInvalidoException):
+        Acesso("AM12","25:00",'20:50')
+        Acesso("AM12","20:60",'20:50')
+        Acesso("AM12","23:00",'25:50')
+        Acesso("AM12","21:00",'20:611')
+        Acesso("AM12","-21:00",'20:11')
